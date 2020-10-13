81818b9a514f7c7a8564c1d1debf443c2e1f21c3
Michiel Coulier <michielcoulier@gmail.com>
2020-10-13 16:55:21 +0200

    added comment

--- Blackjack.php
+++ Blackjack.php
@@ -7,15 +7,7 @@ class Blackjack
     private $dealer;
     private $deck;
 
-    public function getPlayer(): Player
-    {
-        return $this->player;
-    }
-    public function getDealer()
-    {
-        return $this->dealer;
-    }
-
+//Create deck, shuffle deck, distribute deck
     public function __construct()
     {
         $this->deck = new Deck;
@@ -23,4 +15,21 @@ class Blackjack
         $this->player = new Player($this->deck);
         $this->dealer = new Player($this->deck);
     }
+
+    public function getPlayer(): Player
+    {
+        return $this->player;
+    }
+
+    public function getDealer()
+    {
+        return $this->dealer;
+    }
+
+    public function getDeck()
+    {
+        return $this->deck;
+    }
+
+
 }
